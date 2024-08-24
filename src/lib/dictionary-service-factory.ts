86d2be0bfd21a1c7ff573e1c31f0ebb537e7dfc3
Ahmed S. Kandeel <KandeelAS@alrajhibank.com.sa>
2024-08-24 16:38:57 +0300

    commit

--- src/lib/dictionary-service-factory.ts
+++ src/lib/dictionary-service-factory.ts
@@ -31,22 +31,5 @@ export class DictionaryServiceFactory {
         });
   }
 }
-
 /** DictionaryServiceFactory singleton */
 export const dictionaryServiceFactory = new DictionaryServiceFactory();
-
- /*
- for  root item id
-            The Dictionary Service needs a root item ID in order to fetch dictionary phrases for the current app. 
-            When not provided, the service will attempt to figure out the root item for the current JSS App using GraphQL and app name.
-            For SXAs site(s) and multisite setup there's no need to specify it - it will be autoresolved.
-            Otherwise, if your Sitecore instance only has 1 JSS App (i.e. in a Sitecore XP setup), you can specify the root item ID here.
-            GraphQL endpoint may reach its rate limit with the amount of requests it receives and throw a rate limit error.
-            GraphQL Dictionary and Layout Services can handle rate limit errors from server and attempt a retry on requests.
-            For this, specify the number of 'retries' the GraphQL client will attempt.
-            By default it is set to 3. You can disable it by configuring it to 0 for this service.
-
-            Additionally, you have the flexibility to customize the retry strategy by passing a 'retryStrategy'.
-            By default it uses the `DefaultRetryStrategy` with exponential back-off factor of 2 and handles error codes 429,
-            502, 503, 504, 520, 521, 522, 523, 524, 'ECONNRESET', 'ETIMEDOUT' and 'EPROTO' . You can use this class or your own implementation of `RetryStrategy`.
-          */
